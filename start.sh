f3df766b84f90e79d8ca05d7c97ac47772b7d033
Scott <dikowitz@gmail.com>
2016-06-04 13:16:15 -0400

    remove mongod from start script.

--- start.sh
+++ start.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 onINT() {
-echo "Killing mongod $mongodPID too"
-kill "$mongodPID"
 kill "$redisPID"
 exit
 }
 
 trap "onINT" SIGINT
-mongod &
-mongodPID="$!"
 redis-server &
 redisPID="$!"
 node app.js
