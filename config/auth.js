356aa822feb600cc548a6e78d93d3cd579e3f127
Scott <dikowitz@gmail.com>
2016-06-04 15:02:04 -0400

    use correct fb callback.

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         clientID      : '999691146773274', // your App ID
         clientSecret  : '91a472b1d2e3ea67f9721969ab0bf105', // your App Secret
-        callbackURL   : 'http://localhost:8000/auth/facebook/callback',
+        callbackURL   : 'http://grouplyapi.herokuapp.com/auth/facebook/callback',
         profileFields: ['email', 'name', 'displayName']
     }
 };
