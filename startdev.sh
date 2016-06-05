43b41a07b23338e85413b2777405c314273c221f
Scott <dikowitz@gmail.com>
2016-06-04 21:36:54 -0400

    use correct api server for local development.

--- startdev.sh
+++ startdev.sh
@@ -11,5 +11,5 @@ mongod &
 mongodPID="$!"
 redis-server &
 redisPID="$!"
-NODE_ENV=development UI_SERVER=http://localhost:5000 API_SERVER=http://grouplyapi.herokuapp.com node app.js
+NODE_ENV=development UI_SERVER=http://localhost:5000 API_SERVER=http://localhost:8000 node app.js
 echo Done
